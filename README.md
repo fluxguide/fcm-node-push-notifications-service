b97a32ead36e5464ed1affe0263a0799ec11fce3
Martin <martin@fluxguide.com>
2018-10-08 16:56:30 +0200

    move to commander instead of argparse. add README.md

---
+++ README.md
@@ -0,0 +1,85 @@
+# FCM Push Notification Service
+
+## Installation
+
+### 1. Install node modules. In this directory, run: 
+
+```
+npm install
+```
+
+### 2. Get service_account.json file
+
+Download service_accounts.json file from [Firebase Console](https://console.firebase.google.com) → Your Project → Settings → Service Accounts → **Generate new private key**
+
+Copy this file (`service_account.json`) to this directory.
+
+Copy the `databaseURL` and save it somewhere temporarily (used in step 3)
+
+### 3. Create .env file
+
+Duplicate the sample.env and rename it to `.env`
+
+Set these values:
+
+- `SERVICE_ACCOUNT_JSON`: path to the service_accounts.json file
+- `DATABASE_URL`: databaseURL copied in step 2
+
+
+## Usage
+
+### Output help
+
+```
+node send.js -h
+```
+
+will output: 
+
+```
+FCM Push Notification Service
+
+Options:
+
+  -V, --version                        output the version number
+  --title <title>                      The title of the message
+  --message <message>                  The message of the message
+  --device-id [device-id]              Send to a single device
+  --topic [topic]                      The topic to send to (if no device-id is used)
+  --notification-id [notification-id]  Optional notification id (default: 1)
+  --dry                                Dry-run, do not send the message
+  --jsondata [jsondata]                Stringified json to pass in otherData
+  -h, --help                           output usage information
+
+```
+
+### Example: Sending to all users of one topic
+
+```
+node send.js --message "My Message" --title "My Title" --topic "my_topic"
+```
+
+
+### Example: Sending to one specific device
+
+if you know the registrationID of your device, you can send a message to this device specifically.
+
+```
+node send.js --message "My Message" --title "My Title" --device-id "clUtNR_APoo:AP.........."
+```
+
+### Other options
+
+**Custom Payload:** `--jsondata "{\"foo\": \"bar\"}"`
+
+Will be available in event-data as `data.additionalData.otherData.foo = "bar"`
+
+**Notification ID:** `--notification-id "142"`
+
+Will be available in event-data as `data.additionalData.notId`
+
+
+**Dry Run:** `--dry`
+
+Will not send the notification.
+
--- send.js
+++ send.js
@@ -1,64 +1,37 @@
 #!/usr/bin/env node
 require('dotenv').config()
 const admin = require("firebase-admin");
-
+const commander = require('commander');
 
 /**
- * ArgumentParser
+ * Argument Parsing with commander
  */
-var ArgumentParser = require('argparse').ArgumentParser;
-var parser = new ArgumentParser({
-    version: '0.0.1',
-    addHelp: true,
-    description: 'Node Push Server'
-});
-parser.addArgument(
-    ['-t', '--title'],
-    {
-        help: 'The title of the message',
-        required: true,
-        defaultValue: ""
-    }
-);
-parser.addArgument(
-    ['-m', '--message'],
-    {
-        help: 'The message of the message',
-        required: true,
-        defaultValue: ""
-    }
-);
-parser.addArgument(
-    ['--topic'],
-    {
-        help: 'The topic to send to',
-    }
-);
-parser.addArgument(
-    ['--device-id'],
-    {
-        help: 'Send to a single device (no topic)',
-    }
-);
-parser.addArgument(
-    ['--dry', '--dry-run'],
-    {
-        help: 'dry run (do not send)',
-        defaultValue: false
-    }
-);
+commander
+    .version('0.1.0')
+    .description('FCM Push Notification Service')
+    .option('--title <title>', 'The title of the message')
+    .option('--message <message>', 'The message of the message')
+    .option('--device-id [device-id]', 'Send to a single device')
+    .option('--topic [topic]', 'The topic to send to (if no device-id is used)')
+    .option('--notification-id [notification-id]', 'Optional notification id', '1')
+    .option('--dry', 'Dry-run, do not send the message')
+    .option('--jsondata [jsondata]', 'Stringified json to pass in otherData')
+    .parse(process.argv);
 
-var args = parser.parseArgs();
 
-// use dryRun? (do not send message)
-var dryRun = (args.dry == 'true') ? true : false;
+/** 
+ * Validate JSON Data 
+ * */
+let otherData = {};
+if(commander.jsondata) otherData = JSON.parse(commander.jsondata);
+otherData = JSON.stringify(otherData);
 
 
 
 /**
  * Setup FCM admin
  */
-var serviceAccount = require(process.env.SERVICE_ACCOUNT_JSON);
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_JSON);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -68,20 +41,18 @@ admin.initializeApp({
 
 
 
-
 /** 
- * Define Payload for both platforms
+ * Define Payload for Android + iOS
  */
-
-var message = {
+let message = {
     android: {
         ttl: 3600 * 1000, // 1 hour in milliseconds
         priority: 'normal',
         data: {
-            "title": args.title,
-            "body": args.message,
-            "notId": "10",
-            "otherData": "blabla"
+            title: commander.title,
+            body: commander.message,
+            notId: commander.notificationId,
+            otherData: otherData
         }
     },
     apns: {
@@ -91,34 +62,34 @@ var message = {
         payload: {
             aps: {
                 alert: {
-                    title: args.title,
-                    body: args.message,
+                    title: commander.title,
+                    body: commander.message,
                 },
                 "content-available": '1'
             },
-            "notId": "10",
-            "otherData": "blabla",
+            notId: commander.notificationId,
+            otherData: otherData,
         }, 
     },
 }
 
 
+
 /**
  * Define if send to topic (if set) or device
  */
-if (args.device_id) {
+if (commander.deviceId) {
     // This registration token comes from the client FCM SDKs.
-    message.token = args.device_id;
+    message.token = commander.deviceId;
 }
-else if(args.topic) {
-    message.topic = args.topic;
+else if(commander.topic) {
+    message.topic = commander.topic;
 }
 
 
 
-// Send a message to the device corresponding to the provided
-// registration token.
-admin.messaging().send(message, dryRun)
+// Send a message to the device or topic
+admin.messaging().send(message, commander.dry)
   .then((response) => {
     // Response is a message ID string.
     console.log('Successfully sent message:', response);
